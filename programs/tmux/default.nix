9ae3456ecc382c3bd066f7529e23b9c5f3aaaa2c
bodymindarts <justin@galoy.io>
2024-10-22 21:33:26 +0200

    chore: set zsh in tmux

--- programs/tmux/default.nix
+++ programs/tmux/default.nix
@@ -4,4 +4,8 @@
   clock24 = true;
   historyLimit = 5000;
   terminal = "screen-256color";
+  shell = "${pkgs.zsh}/bin/zsh";
+  extraConfig = ''
+    set -g default-command "${pkgs.zsh}/bin/zsh"
+  '';
 }
