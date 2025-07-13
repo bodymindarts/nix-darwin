dab0af8fed3bcc62bc25acab9b6036a5f65e4102
bodymindarts <justin@galoy.io>
2025-07-13 06:25:56 +0200

    chore: bump flake

--- home-manager.nix
+++ home-manager.nix
@@ -17,10 +17,10 @@
         pkgs.tree
 
         (pkgs.claude-code.overrideAttrs (oldAttrs: rec {
-          version = "1.0.31";
+          version = "1.0.51";
           src = pkgs.fetchzip {
             url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-            hash = "sha256-DrjR1LHnLXk2cW3zXO1jxc3octITMq6t28E2KJHBxZE=";
+            hash = "sha256-sAILRsi8ZViMfcpqykfnFQzHTJHRwRSZz45otMqa4U0=";
           };
           npmDepsHash = "";
         }))
