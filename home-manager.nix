71ded8385d1325e81364129599fa41ed4f945b30
bodymindarts <justin@galoy.io>
2025-08-24 13:11:51 +0200

    chore: bumo claude

--- home-manager.nix
+++ home-manager.nix
@@ -17,10 +17,10 @@
         pkgs.tree
 
         (pkgs.claude-code.overrideAttrs (oldAttrs: rec {
-          version = "1.0.51";
+          version = "1.0.89";
           src = pkgs.fetchzip {
             url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-            hash = "sha256-sAILRsi8ZViMfcpqykfnFQzHTJHRwRSZz45otMqa4U0=";
+            hash = "sha256-ULOL4emd7VZNvHD5Hk3YI1Cyb1Q1YMCcDDxlRDUM1dc=";
           };
           npmDepsHash = "";
         }))
--- programs/neovim/default.nix
+++ programs/neovim/default.nix
@@ -12,9 +12,12 @@
         let g:ale_fixers = {'javascript': [], 'typescript': ['eslint'], 'typescript.tsx': ['eslint'], 'rust': ['rustfmt'] }
         let g:ale_lint_on_text_changed = 'normal'
         let g:ale_lint_on_insert_leave = 1
-        let g:ale_lint_delay = 200
+        let g:ale_lint_delay = 0
         let g:ale_set_quickfix = 0
         let g:ale_set_loclist = 0
+        let g:ale_set_highlights = 0
+        let g:ale_set_signs = 1
+        let g:ale_virtualtext_cursor = 0
         let g:ale_javascript_eslint_options = '--cache'
         let g:ale_javascript_eslint_executable = 'eslint_d'
         let g:ale_fix_on_save = 1
