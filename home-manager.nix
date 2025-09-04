8e687e24e0f907be5c915541f27e20273a6b6c58
bodymindarts <justin@galoy.io>
2025-09-04 20:20:52 +0200

    chore: bump claude

--- home-manager.nix
+++ home-manager.nix
@@ -17,10 +17,10 @@
         pkgs.tree
 
         (pkgs.claude-code.overrideAttrs (oldAttrs: rec {
-          version = "1.0.89";
+          version = "1.0.105";
           src = pkgs.fetchzip {
             url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-            hash = "sha256-ULOL4emd7VZNvHD5Hk3YI1Cyb1Q1YMCcDDxlRDUM1dc=";
+            hash = "sha256-9beqR/jIBazW6y9yw1cgJOESrrSmbKdRLyH5Vg2Uss8=";
           };
           npmDepsHash = "";
         }))
